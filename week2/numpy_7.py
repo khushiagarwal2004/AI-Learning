49c9a785e03c265db8433fed95ddafa77879775c
khushiagarwal2004 <khushi.agarwal0412@gmail.com>
2026-04-07 04:02:37 -0700

    Numpy + Matplotlib + seaborn

--- week2/numpy_7.py
+++ week2/numpy_7.py
@@ -1,16 +1,73 @@
+# import numpy as np
+
+# sigmoid : Σ(x) = 1 / (1 + e^(-x)) : Convolution me activation function
+def sigmoid(array):
+    return np.round(1/(1+np.exp(-array)),4)
+
+a=np.arange(10)
+print(a)
+print(sigmoid(a))
+
+# mean squared error : mean(actual-predicted)^2) : ML mei Loss Function(with what value we inc/dec weights to reduce error) : Linear Regression 
+actual=np.random.randint(1,50,25)
+predicted=np.random.randint(1,50,25)
+print("\n",actual)
+print(predicted)
+
+def mean_squared_error(actual,predicted):
+    return np.mean((actual-predicted)**2)
+print(mean_squared_error(actual,predicted))
+
+# binary cross entropy
+actual=np.random.randint(0,2,25)
+predicted=np.random.random(25)
+def bin_cross_entropy(actual,predicted):
+    return -np.mean(actual*np.log(predicted)+(1-actual)*np.log(1-predicted))
+
+print(bin_cross_entropy(actual,predicted))
+
+# isnan
+a = np.array([1,2,3,4,5,np.nan,6])
+print(a)
+print(a[~np.isnan(a)])
+
+# # Graphs
+import matplotlib.pyplot as plt
 import numpy as np
 
-# Broadcasting : It tells how numpy treats arrays with different shapes during arithmetic operations.
-# The smaller array is broadcasted across larger array so that they have compatible shapes.
-a=np.arange(6).reshape(2,3)
-b=np.arange(3).reshape(1,3)
-print(a,"\n")
-print(b,"\n")
-c=a+b
-print(c,"\n") # Gives sum as : b is added in both rows of a
+# y=x
+x=np.linspace(-10,10,100)
+y=x
+plt.plot(x,y)
+plt.show()
 
-# Broadcasting Rules
-# 1. Make 2 arrays of same dimensions (eg.: if a(3,3) and b(3) then make b as b(1,3))
-# 2. Make size of smaller array same as size of array a (eg.: if a(3,3) and b(3) then make b as b(3,3))
-# 3. Only strech 1 to make smaller array equal to big one
+# y=x^2
+x=np.linspace(-10,10,100)
+y=x**2
+plt.plot(x,y)
+plt.show()
 
+# y=sin(x)
+x=np.linspace(-10,10,100)
+y=np.sin(x)
+plt.plot(x,y)
+plt.show()
+
+# y=xlog(x)
+x=np.linspace(-10,10,100)
+y=x*np.log(x)
+plt.plot(x,y)
+plt.show()
+
+# sigmoid graph
+x=np.linspace(-10,10,100)
+y=1/(1+np.exp(-x))
+plt.plot(x,y)
+plt.show()
+
+# seaborn
+import seaborn as sns 
+sns.displot([0,1,2,3,4,5])
+plt.show()
+sns.displot([0,1,2,3,4,5],kind="kde")
+plt.show()
